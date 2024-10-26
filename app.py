c71149fa311e6efd8b7ce644cd4578a5badfd970
rawanamahmoud <146339190+rawanamahmoud@users.noreply.github.com>
2024-10-26 23:30:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if os.path.exists('./dataset.csv'):
 
 with st.sidebar: 
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("AutoNickML")
+    st.title("AutoRawanML")
     choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
